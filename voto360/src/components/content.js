4b2fa08255f2f898a685fd163125ff5d086a6575
abalioni <balionisalexia@gmail.com>
2018-05-05 20:33:05 -0300

    Listagem de Pesquisas de votos

--- voto360/src/components/content.js
+++ voto360/src/components/content.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 import {Admin} from './admin'
 import Login from './login'
@@ -18,139 +18,186 @@ import Divider from 'material-ui/Divider';
 import Subheader from 'material-ui/Subheader';
 import ComparacaoPoliticos from './comparacaoPoliticos';
 import CriarPesquisa from './criarPesquisa'
+import ListaPesquisas from './listaPesquisas'
 
-import { cookie } from 'cookie_js'
-
+import {cookie} from 'cookie_js'
 
 // export class Content = ({match, isLoggedIn}) => {
 class Content extends Component {
   constructor(props) {
     super(props)
-    
+
     this.state = {
       isLoggedIn: cookie.get('user')
     }
-    
+
     this.handleLogout = this.handleLogout.bind(this)
     this.renderLink = this.renderLink.bind(this)
   }
-  
+
   handleLogged(isLoggedIn) {
-    this.setState({
-      isLoggedIn
-    })
+    this.setState({isLoggedIn})
   }
-  
+
   handleLogout() {
     const user = cookie.get('user');
-    
-    if(user){
+
+    if (user) {
       console.log("logout");
       cookie.remove('user');
     }
     this.handleLogged(false)
   }
-  
+
   renderLink(user = {}) {
-    
+
     if ((this.state.isLoggedIn && user.cargo === 'politico')) {
-      return (<Link style={{ textDecoration: 'none' }} to="/alteraCadastroPolitico">
+      return (<Link style={{
+          textDecoration: 'none'
+        }} to="/alteraCadastroPolitico">
         <MenuItem onClick={this.props.handleClose}>
           Alterar Cadastro Politico
-      </MenuItem>
+        </MenuItem>
       </Link>)
     } else if (this.state.isLoggedIn && user.cargo !== 'admin' && user.cargo !== 'editor') {
-      return (<Link style={{ textDecoration: 'none' }} to="/meusDados">
-      <MenuItem onClick={this.props.handleClose}>
-      Alterar Cadastro
-      </MenuItem>
+      return (<Link style={{
+          textDecoration: 'none'
+        }} to="/meusDados">
+        <MenuItem onClick={this.props.handleClose}>
+          Alterar Cadastro
+        </MenuItem>
       </Link>)
-    } 
+    }
     return null
   }
-  
+
   render() {
     const cookieUser = cookie.get('user');
     let user;
-    if(cookieUser){
+    if (cookieUser) {
       user = JSON.parse(cookieUser);
     }
     return (
-      // <Router>
-      <div>
-      <Drawer width={200} open={this.props.open} onRequestChange={this.props.handleToggle} >
-  
-      <Subheader>VOTO360</Subheader>
-      {user && (user.cargo === 'admin' || user.cargo === 'editor') ? undefined : (<Link style={{textDecoration: 'none'}} to="/">
-      
-      <MenuItem onClick={this.props.handleClose}>
-          Home
-      </MenuItem></Link>)}
-    
-    <Divider />
-     <Subheader>Sobre Políticos</Subheader>
-    {user && (user.cargo === 'admin'|| user.cargo === 'editor') ? undefined : (<Link style={{textDecoration: 'none'}} to="/comparacaoPoliticos">
-    <MenuItem onClick={this.props.handleClose}>
-      Comparação Politicos
-    </MenuItem>
-    </Link>) }
-    
-    
-    {user && (user.cargo === 'admin' || user.cargo === 'editor') ? undefined : (<Link style={{textDecoration: 'none'}} to="/pesquisasDeVoto">
-    <MenuItem onClick={this.props.handleClose}>
-    Pesquisas de Voto
-    </MenuItem>
-    </Link>)}
-    
-    <Divider />
-    
-    {user && user.cargo === 'admin' && <Link style={{textDecoration: 'none'}} to="/admin">
-    <MenuItem onClick={this.props.handleClose}>
-    Admin
-    </MenuItem>
-    </Link>}
-    
-    <Divider />
-    
-    {user && user.cargo === 'editor' && <Link style={{textDecoration: 'none'}} to="/PoliticsRequests">
-    <MenuItem onClick={this.props.handleClose}>
-      Aprovar Perfil Politico
-    </MenuItem>
-    </Link>}
-    
-    <Divider />
-    <Subheader>Meu perfil</Subheader>
-    { this.renderLink(user) }
-    
-    {this.state.isLoggedIn ? null : (<Link style={{textDecoration: 'none'}} to="/login">
-    <MenuItem onClick={this.props.handleClose}>
-    Login/Cadastro
-    </MenuItem></Link>)}
-    {this.state.isLoggedIn ? (<FlatButton fullWidth={true} onClick={this.handleLogout}>
-      <MenuItem onClick={this.props.handleClose}>
-      Logout
-      </MenuItem>
-      </ FlatButton >) : null}
-      </Drawer>
-      
-      
-      <Route exact path="/" component={Home}/>
-      <Route path="/pesquisasDeVoto" component={Pesquisa}/>
-      <Route path="/admin" render={(props) => (user && user.cargo === 'admin') ? <Admin {...props} /> : <div></div>} />
-      <Route path="/PoliticsRequests" render={(props) => (user && user.cargo === 'editor') ? <PoliticsRequests {...props} /> : <div></div>} />
-      <Route path="/forgotpassword/" component={NotLoggedReset}/>
-      <Route path="/login" render={(props) => <Login handleLogin={() => {this.handleLogged(true)}} {...props} />} />
-      <Route path="/verify-change-password-token/:token" component={VerifyChangePasswordToken} />
-      <Route path="/meusDados" render={(props) => (user && user.cargo !== 'admin') ? <MeusDados {...props} /> : <div></div>} />
-      <Route path="/cadastroPolitico" render={(props) => (user && user.cargo !== 'admin') ? <CadastroPolitico {...props} /> : <div></div>} />
-      <Route path="/alteraCadastroPolitico" render={(props) => (user && (user.cargo === 'politico')) ? <AlteraCadastroPolitico {...props} /> : <div></div>} />
-      <Route path="/comparacaoPoliticos" component={ComparacaoPoliticos} />
-      <Route path="/criarPesquisa" component={CriarPesquisa} />
+    <div>
+      <Drawer width={200} open={this.props.open} onRequestChange={this.props.handleToggle}>
+        <Subheader>VOTO360</Subheader>
+        {
+          user && (user.cargo === 'admin' || user.cargo === 'editor')
+            ? null
+            : (<Link style={{
+                textDecoration: 'none'
+              }} to="/">
 
-      </div>
-      // </Router>
-    )}
+              <MenuItem onClick={this.props.handleClose}>
+                Home
+              </MenuItem>
+            </Link>)
+        }
+
+        <Divider/>
+        <Subheader>Sobre Políticos</Subheader>
+        {
+          user && (user.cargo === 'admin' || user.cargo === 'editor')
+            ? null
+            : (<Link style={{
+                textDecoration: 'none'
+              }} to="/comparacaoPoliticos">
+              <MenuItem onClick={this.props.handleClose}>
+                Comparação Politicos
+              </MenuItem>
+            </Link>)
+        }
+
+        {
+          user && user.cargo === 'admin' && <Link style={{
+                textDecoration: 'none'
+              }} to="/admin">
+              <MenuItem onClick={this.props.handleClose}>
+                Admin
+              </MenuItem>
+            </Link>
+        }
+
+        <Divider/> {
+          user && user.cargo === 'editor' && <Link style={{
+                textDecoration: 'none'
+              }} to="/PoliticsRequests">
+              <MenuItem onClick={this.props.handleClose}>
+                Aprovar Perfil Politico
+              </MenuItem>
+            </Link>
+        }
+
+        <Divider/> {
+          user && user.cargo === 'editor' && (<Link style={{
+              textDecoration: 'none'
+            }} to="/listaPesquisas">
+            <MenuItem onClick={this.props.handleClose}>
+              Pesquisas de Voto
+            </MenuItem>
+          </Link>)
+        }
+
+        <Divider/>
+        <Subheader>Meu perfil</Subheader>
+        {this.renderLink(user)}
+
+        {
+          this.state.isLoggedIn
+            ? null
+            : (<Link style={{
+                textDecoration: 'none'
+              }} to="/login">
+              <MenuItem onClick={this.props.handleClose}>
+                Login/Cadastro
+              </MenuItem>
+            </Link>)
+        }
+        {
+          this.state.isLoggedIn
+            ? (<FlatButton fullWidth={true} onClick={this.handleLogout}>
+              <MenuItem onClick={this.props.handleClose}>
+                Logout
+              </MenuItem>
+            </ FlatButton >)
+            : null
+        }
+      </Drawer>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/admin" render={(
+          props) => (user && user.cargo === 'admin')
+          ? <Admin {...props}/>
+          : null}/>
+      <Route path="/PoliticsRequests" render={(
+          props) => (user && user.cargo === 'editor')
+          ? <PoliticsRequests {...props}/>
+          : null}/>
+      <Route path="/forgotpassword/" component={NotLoggedReset}/>
+      <Route path="/login" render={(props) => <Login handleLogin={() => {
+            this.handleLogged(true)
+          }} {...props}/>}/>
+      <Route path="/verify-change-password-token/:token" component={VerifyChangePasswordToken}/>
+      <Route path="/meusDados" render={(
+          props) => (user && user.cargo !== 'admin')
+          ? <MeusDados {...props}/>
+          : null}/>
+      <Route path="/cadastroPolitico" render={(
+          props) => (user && user.cargo !== 'admin')
+          ? <CadastroPolitico {...props}/>
+          : null}/>
+      <Route path="/listaPesquisas" render={(
+          props) => (user && user.cargo === 'editor')
+          ? <ListaPesquisas {...props}/>
+          : null}/>
+      <Route path="/alteraCadastroPolitico" render={(
+          props) => (user && (user.cargo === 'politico'))
+          ? <AlteraCadastroPolitico {...props}/>
+          : null}/>
+      <Route path="/comparacaoPoliticos" component={ComparacaoPoliticos}/>
+      <Route path="/criarPesquisa" component={CriarPesquisa}/>
+    </div>)
   }
-  
-  export default Content;
-  
+}
+
+export default Content;
